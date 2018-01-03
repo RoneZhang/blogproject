b32c995a69754edcf1f0a7a32168cc0b1f9ba134
zr <zrpc2009@163.com>
2018-01-03 15:33:24 +0800

    增加了标签相关内容，RSS订阅相关内容

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 from django import template
+from django.db.models import Count
 
 register = template.Library()
 
@@ -16,4 +17,13 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    # return Category.objects.all()
+
+    # 记得在顶部引入 count 函数
+    # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
