ad542cdcbdd59b8f34a6974a87679bf0e1d31e47
RoneZhang <zrpc2009@163.com>
2018-01-05 15:06:14 +0800

    Update settings.py
    DEBUG = False

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#^5o&w2bbzigffa*$v9r@0ib$+y*dd8$4+ojqxpdvbe!4y%u%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '61.50.150.90']
 
